17eacf5edaff54fce8ebdbd90d5592dea74a7534
choquitofrito <zyriab@gmail.com>
2024-07-09 14:38:40 +0200

    finds ok

--- ProjetModeleSymfony/migrations/Version20240709123559.php
+++ ProjetModeleSymfony/migrations/Version20240709123559.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240709114241 extends AbstractMigration
+final class Version20240709123559 extends AbstractMigration
 {
     public function getDescription(): string
     {
