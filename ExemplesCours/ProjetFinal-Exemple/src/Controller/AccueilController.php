fba0a9cdfdd47a40324072fcedb96cebc976638b
choquitofrito <zyriab@gmail.com>
2025-09-18 09:59:26 +0200

    entité Utilisateur créée

--- ExemplesCours/ProjetFinal-Exemple/src/Controller/AccueilController.php
+++ ExemplesCours/ProjetFinal-Exemple/src/Controller/AccueilController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 // importer les entities
 use App\Entity\Animal;
 
